d6eeaec592a9f7bb2f0419040634f4a3b4cfe8ec
Leomaru <33780483+Mahoumaru@users.noreply.github.com>
2020-12-11 18:06:57 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup
 
 setup(name='tmomentum',
-      version='0.1',
+      version='1.0.0',
+      url='https://github.com/Mahoumaru/t-momentum.git',
       install_requires=['torch']
 )
